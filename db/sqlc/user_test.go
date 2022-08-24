fc2998aa901494f4d6224841dbc8be32073e17cc
Dan Rousseau <danrousseau@protonmail.com>
2022-08-24 13:31:48 +0200

    feat: test for organisations crud

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func createRandomUser(t *testing.T) User {
+func CreateRandomUser(t *testing.T) User {
 	arg := CreateUserParams{
 		Name:  util.RandomUsername(),
 		Email: util.RandomEmail(),
@@ -27,71 +27,70 @@ func createRandomUser(t *testing.T) User {
 	require.NotZero(t, user.CreatedAt)
 	require.NotZero(t, user.UpdatedAt)
 
-    return user
+	return user
 }
 
 func TestCreateUser(t *testing.T) {
-    createRandomUser(t)
+	CreateRandomUser(t)
 }
 
 func TestGetUser(t *testing.T) {
-    user1 := createRandomUser(t)
-    user2, err := testQueries.GetUser(context.Background(), user1.ID)
-    require.NoError(t, err)
-    require.NotEmpty(t, user2)
+	user1 := CreateRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
 
-    require.Equal(t, user1.ID, user2.ID)
-    require.Equal(t, user1.Name, user2.Name)
-    require.Equal(t, user1.Email, user2.Email)
-    require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
-    require.WithinDuration(t, user1.UpdatedAt, user2.UpdatedAt, time.Second)
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Name, user2.Name)
+	require.Equal(t, user1.Email, user2.Email)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+	require.WithinDuration(t, user1.UpdatedAt, user2.UpdatedAt, time.Second)
 }
 
 func TestUpdateUser(t *testing.T) {
-    user1 := createRandomUser(t)
+	user1 := CreateRandomUser(t)
 
-    arg := UpdateUserParams{
-        ID:    user1.ID,
-        Name:  util.RandomUsername(),
-    }
+	arg := UpdateUserParams{
+		ID:   user1.ID,
+		Name: util.RandomUsername(),
+	}
 
-    user2, err := testQueries.UpdateUser(context.Background(), arg)
-    require.NoError(t, err)
-    require.NotEmpty(t, user2)
+	user2, err := testQueries.UpdateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
 
-    require.Equal(t, arg.ID, user2.ID)
-    require.Equal(t, arg.Name, user2.Name)
-    require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
-    require.WithinDuration(t, user2.UpdatedAt, user2.UpdatedAt, time.Second)
+	require.Equal(t, arg.ID, user2.ID)
+	require.Equal(t, arg.Name, user2.Name)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+	require.WithinDuration(t, user2.UpdatedAt, user2.UpdatedAt, time.Second)
 }
 
 func TestDeleteUser(t *testing.T) {
-    user1 := createRandomUser(t)
+	user1 := CreateRandomUser(t)
 
-    err := testQueries.DeleteUser(context.Background(), user1.ID)
-    require.NoError(t, err)
+	err := testQueries.DeleteUser(context.Background(), user1.ID)
+	require.NoError(t, err)
 
-    user2, err := testQueries.GetUser(context.Background(), user1.ID)
-    require.Error(t, err)
-    require.Empty(t, user2)
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+	require.Error(t, err)
+	require.Empty(t, user2)
 }
 
 func TestListUsers(t *testing.T) {
-    for i := 0; i < 10; i++ {
-        createRandomUser(t)
-    }
+	for i := 0; i < 10; i++ {
+		CreateRandomUser(t)
+	}
 
-    arg := ListUsersParams{
-        Limit:  5,
-        Offset: 5,
-    }
+	arg := ListUsersParams{
+		Limit:  5,
+		Offset: 5,
+	}
 
-    users, err := testQueries.ListUsers(context.Background(), arg)
-    require.NoError(t, err)
-    require.Len(t, users, 5)
+	users, err := testQueries.ListUsers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, users, 5)
 
-    for _, user := range users {
-        require.NotEmpty(t, user)
-    }
+	for _, user := range users {
+		require.NotEmpty(t, user)
+	}
 }
-
