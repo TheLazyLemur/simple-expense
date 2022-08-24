352838ee1bf4c499ad151d5e74955223c4be18d0
Dan Rousseau <danrousseau@protonmail.com>
2022-08-24 11:55:24 +0200

    feat: all tests for user creation crud implemented

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -3,14 +3,17 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
+
+	"TheLazyLemur/simple-expense/util"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateUser(t *testing.T) {
+func createRandomUser(t *testing.T) User {
 	arg := CreateUserParams{
-		Name:  "Dan",
-		Email: "danrousseau@protonmail.com",
+		Name:  util.RandomUsername(),
+		Email: util.RandomEmail(),
 	}
 
 	user, err := testQueries.CreateUser(context.Background(), arg)
@@ -23,4 +26,72 @@ func TestCreateUser(t *testing.T) {
 	require.NotZero(t, user.ID)
 	require.NotZero(t, user.CreatedAt)
 	require.NotZero(t, user.UpdatedAt)
+
+    return user
 }
+
+func TestCreateUser(t *testing.T) {
+    createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+    user1 := createRandomUser(t)
+    user2, err := testQueries.GetUser(context.Background(), user1.ID)
+    require.NoError(t, err)
+    require.NotEmpty(t, user2)
+
+    require.Equal(t, user1.ID, user2.ID)
+    require.Equal(t, user1.Name, user2.Name)
+    require.Equal(t, user1.Email, user2.Email)
+    require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+    require.WithinDuration(t, user1.UpdatedAt, user2.UpdatedAt, time.Second)
+}
+
+func TestUpdateUser(t *testing.T) {
+    user1 := createRandomUser(t)
+
+    arg := UpdateUserParams{
+        ID:    user1.ID,
+        Name:  util.RandomUsername(),
+    }
+
+    user2, err := testQueries.UpdateUser(context.Background(), arg)
+    require.NoError(t, err)
+    require.NotEmpty(t, user2)
+
+    require.Equal(t, arg.ID, user2.ID)
+    require.Equal(t, arg.Name, user2.Name)
+    require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+    require.WithinDuration(t, user2.UpdatedAt, user2.UpdatedAt, time.Second)
+}
+
+func TestDeleteUser(t *testing.T) {
+    user1 := createRandomUser(t)
+
+    err := testQueries.DeleteUser(context.Background(), user1.ID)
+    require.NoError(t, err)
+
+    user2, err := testQueries.GetUser(context.Background(), user1.ID)
+    require.Error(t, err)
+    require.Empty(t, user2)
+}
+
+func TestListUsers(t *testing.T) {
+    for i := 0; i < 10; i++ {
+        createRandomUser(t)
+    }
+
+    arg := ListUsersParams{
+        Limit:  5,
+        Offset: 5,
+    }
+
+    users, err := testQueries.ListUsers(context.Background(), arg)
+    require.NoError(t, err)
+    require.Len(t, users, 5)
+
+    for _, user := range users {
+        require.NotEmpty(t, user)
+    }
+}
+
