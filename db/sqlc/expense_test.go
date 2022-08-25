c3db703a2f0d331cc195fcd547562dcb1b130398
Dan Rousseau <danrousseau@protonmail.com>
2022-08-25 22:15:21 +0200

    feat: added missing tests for expenses and invoices

--- db/sqlc/expense_test.go
+++ db/sqlc/expense_test.go
@@ -33,3 +33,44 @@ func CreateRandomExpense(t *testing.T) Expense {
 func TestCreateExpense(t *testing.T) {
 	CreateRandomExpense(t)
 }
+
+func TestGetExpense(t *testing.T) {
+    expense1 := CreateRandomExpense(t)
+    expense2, err := testQueries.GetExpense(context.Background(), expense1.ID)
+    require.NoError(t, err)
+    require.NotEmpty(t, expense2)
+
+    require.Equal(t, expense1.ID, expense2.ID)
+    require.Equal(t, expense1.Uploader, expense2.Uploader)
+    require.Equal(t, expense1.Amount, expense2.Amount)
+    require.Equal(t, expense1.OrganisationID, expense2.OrganisationID)
+}
+
+func TestListExpense(t *testing.T) {
+    for i := 0; i < 10; i++ {
+        CreateRandomExpense(t)
+    }
+
+    arg := ListExpenseParams{
+        Limit:  5,
+        Offset: 5,
+    }
+
+    expenses, err := testQueries.ListExpense(context.Background(), arg)
+    require.NoError(t, err)
+    require.Len(t, expenses, 5)
+
+    for _, expense := range expenses {
+        require.NotEmpty(t, expense)
+    }
+}
+
+func TestDeleteExpense(t *testing.T) {
+    expense1 := CreateRandomExpense(t)
+    err := testQueries.DeleteExpense(context.Background(), expense1.ID)
+    require.NoError(t, err)
+
+    expense2, err := testQueries.GetExpense(context.Background(), expense1.ID)
+    require.Error(t, err)
+    require.Empty(t, expense2)
+}
