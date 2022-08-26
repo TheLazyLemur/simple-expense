50e5577c893456f392462e31a027f8dc8817e726
Dan Rousseau <danrousseau@protonmail.com>
2022-08-26 22:25:02 +0200

    feat: added user handler harcoded post and hardcoded get

--- api/server.go
+++ api/server.go
@@ -4,6 +4,7 @@ import (
 	db "TheLazyLemur/simple-expense/db/sqlc"
 	"TheLazyLemur/simple-expense/util"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,16 +14,35 @@ type Server struct {
 }
 
 func NewServer(store *db.Store) *Server {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		arg := db.CreateUserParams{
-			Name:     util.RandomUsername(),
-			Email:    util.RandomEmail(),
-			Password: util.RandomString(8),
-			Username: util.RandomUsername(),
-			Salt:     util.RandomString(10),
-		}
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
 
-		_, _ = store.CreateUser(context.Background(), arg)
+        if r.Method == http.MethodPost {
+            arg := db.CreateUserParams{
+                Name:     util.RandomUsername(),
+                Email:    util.RandomEmail(),
+                Password: util.RandomString(8),
+                Username: util.RandomUsername(),
+                Salt:     util.RandomString(10),
+            }
+
+            user, _ := store.CreateUser(context.Background(), arg)
+
+            // convert user to json
+            json, _ := json.Marshal(user)
+            w.Write(json)
+        }
+
+        if r.Method == http.MethodGet {
+            user, err := store.GetUser(context.Background(), 127)
+            if err != nil {
+                fmt.Println(err)
+            }
+
+            // convert user to json
+            json, _ := json.Marshal(user)
+            w.Write(json)
+        }
+
 	})
 
 	return &Server{store: store}
