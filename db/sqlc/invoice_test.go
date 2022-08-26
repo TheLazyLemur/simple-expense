a81b31c8761390fc6c2dbb9a719cf9a46f83a573
Dan Rousseau <danrousseau@protonmail.com>
2022-08-26 21:57:11 +0200

    chore: regenerated sqlc

--- db/sqlc/invoice_test.go
+++ db/sqlc/invoice_test.go
@@ -32,43 +32,43 @@ func TestCreateInvoice(t *testing.T) {
 }
 
 func TestGetInvoice(t *testing.T) {
-    invoice1 := CreateRandomInvoice(t)
-    invoice2, err := testQueries.GetInvoice(context.Background(), invoice1.ID)
-    require.NoError(t, err)
-    require.NotEmpty(t, invoice2)
+	invoice1 := CreateRandomInvoice(t)
+	invoice2, err := testQueries.GetInvoice(context.Background(), invoice1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, invoice2)
 
-    require.Equal(t, invoice1.ID, invoice2.ID)
-    require.Equal(t, invoice1.OrganisationID, invoice2.OrganisationID)
-    require.Equal(t, invoice1.Uploader, invoice2.Uploader)
-    require.Equal(t, invoice1.ExpenseID, invoice2.ExpenseID)
+	require.Equal(t, invoice1.ID, invoice2.ID)
+	require.Equal(t, invoice1.OrganisationID, invoice2.OrganisationID)
+	require.Equal(t, invoice1.Uploader, invoice2.Uploader)
+	require.Equal(t, invoice1.ExpenseID, invoice2.ExpenseID)
 }
 
 func TestListInvoice(t *testing.T) {
-    for i := 0; i < 10; i++ {
-        CreateRandomInvoice(t)
-    }
+	for i := 0; i < 10; i++ {
+		CreateRandomInvoice(t)
+	}
 
-    arg := ListInvoiceParams{
-        Limit:  5,
-        Offset: 5,
-    }
+	arg := ListInvoiceParams{
+		Limit:  5,
+		Offset: 5,
+	}
 
-    invoices, err := testQueries.ListInvoice(context.Background(), arg)
-    require.NoError(t, err)
-    require.Len(t, invoices, 5)
+	invoices, err := testQueries.ListInvoice(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, invoices, 5)
 
-    for _, invoice := range invoices {
-        require.NotEmpty(t, invoice)
-    }
+	for _, invoice := range invoices {
+		require.NotEmpty(t, invoice)
+	}
 }
 
 func TestDeleteInvoice(t *testing.T) {
-    invoice1 := CreateRandomInvoice(t)
+	invoice1 := CreateRandomInvoice(t)
 
-    err := testQueries.DeleteInvoice(context.Background(), invoice1.ID)
-    require.NoError(t, err)
+	err := testQueries.DeleteInvoice(context.Background(), invoice1.ID)
+	require.NoError(t, err)
 
-    invoice2, err := testQueries.GetInvoice(context.Background(), invoice1.ID)
-    require.Error(t, err)
-    require.Empty(t, invoice2)
+	invoice2, err := testQueries.GetInvoice(context.Background(), invoice1.ID)
+	require.Error(t, err)
+	require.Empty(t, invoice2)
 }
